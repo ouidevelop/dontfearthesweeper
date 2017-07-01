477f83306b6b9f86fc668a263dd29e4097a75290
Michael Kurrels <mjkurrels@gmail.com>
2017-06-30 18:27:35 -0700

    adds procfile

--- main_test.go
+++ main_test.go
@@ -43,39 +43,32 @@ var _ = Describe("Main", func() {
 			err := DB.Ping()
 			Expect(err).NotTo(HaveOccurred())
 
-			jsonAlert := []byte(`{"timezone":"America/New_York","times":[{"weekday":0,"nthWeek":1}],"phoneNumber":"8054234224","token":""}`)
-
-			fmt.Println("1")
+			jsonAlert := []byte(`{"timezone":"America/New_York","times":[{"weekday":0,"nthWeek":1}],"phoneNumber":"1234567890","token":""}`)
 
 			clearDB()
 			defer clearDB()
 
-			fmt.Println("2")
 			req := httptest.NewRequest("POST", "/verification/verify", bytes.NewReader(jsonAlert))
 			res := httptest.NewRecorder()
 			MockEnv.VerificationVerifyHandler(res, req)
 			Expect(res.Code).To(Equal(http.StatusOK))
-			fmt.Println("3")
 
 			var nextCall int64
 			err = DB.QueryRow("select NEXT_CALL from alerts").Scan(&nextCall)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(nextCall).To(Equal(int64(1494111600))) //2017-05-06 19:00:00 -0400 EDT
-			fmt.Println("4")
 
 			// 1 second after the next call of the alert
 			done := MockNow(time.Unix(1494111601, 0))
 			defer done()
-			fmt.Println("5")
 
 			FindReadyAlerts(MockEnv.MsgSvc)
 			expected := &MockMessageService{
 				from: "5102414070",
-				to:   "8054234224",
-				body: "Don't forget about street tomorrow!",
+				to:   "1234567890",
+				body: "Don't forget about street sweeping tomorrow! (to stop getting these reminders, please email mjkurrels@gmail.com)",
 			}
 			Expect(MockEnv.MsgSvc).To(Equal(expected))
-			fmt.Println("6")
 		})
 	})
 })
--- twilioAPI.go
+++ twilioAPI.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sfreiberg/gotwilio"
 )
 
+// MessageServicer is out interface for sending messages. This can be mocked in the tests.
 type MessageServicer interface {
 	phoneVerifier
 	smsMessager
@@ -22,23 +23,23 @@ type smsMessager interface {
 	Send(from, to, body string) error
 }
 
-type TwilioMessageService struct {
+type twilioMessageService struct {
 	authy  *authy.Authy
 	twilio *gotwilio.Twilio
 }
 
-func (t *TwilioMessageService) Send(from, to, body string) error {
+func (t *twilioMessageService) Send(from, to, body string) error {
 	_, _, err := t.twilio.SendSMS("+1"+from, "+1"+to, body, "", "") // todo: should not ignore those returns
 	return err
 }
 
-func (t *TwilioMessageService) RequestCode(phoneNumber string) (bool, error) {
+func (t *twilioMessageService) RequestCode(phoneNumber string) (bool, error) {
 	fmt.Println("phoneNumber: ", phoneNumber)
 	verification, err := t.authy.StartPhoneVerification(1, phoneNumber, "sms", url.Values{})
 	return verification.Success, err
 }
 
-func (t *TwilioMessageService) VerifyCode(phoneNumber, code string) (bool, error) {
+func (t *twilioMessageService) VerifyCode(phoneNumber, code string) (bool, error) {
 	verification, err := t.authy.CheckPhoneVerification(1, phoneNumber, code, url.Values{})
 	return verification.Success, err
 }
