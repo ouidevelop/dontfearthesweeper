add0d75f79682e9a2d1d7ad924b71c32861def78
Michael Kurrels <mjkurrels@gmail.com>
2017-08-21 23:38:36 -0700

    changes alert message to tell how to remove

--- main.go
+++ main.go
@@ -285,7 +285,7 @@ func timeAtNthDayOfMonth(t time.Time, nthDay int, weekday int, hour int) time.Ti
 
 func remind(phoneNumber string, sender smsMessager, id int) {
 	fmt.Println("sending message to: ", id)
-	message := "Don't forget about street sweeping tomorrow! (to stop getting these reminders, please email mjkurrels@gmail.com)"
+	message := "Don't forget about street sweeping tomorrow! (to stop getting these reminders, go to dontfearthesweeper.com/remove or email ouidevelop@gmail.com)"
 	err := sender.Send(from, phoneNumber, message)
 	if err != nil {
 		log.Println("problem sending message: ", err)
